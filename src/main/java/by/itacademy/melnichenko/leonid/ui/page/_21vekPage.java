d6b3ed065ebeafb9f5a942acf1273c195dea49b0
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-03 01:27:50 +0300

    create class PageObject

---
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/page/_21vekPage.java
@@ -0,0 +1,39 @@
+package by.itacademy.melnichenko.leonid.ui.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class _21vekPage {
+    private WebDriver webDriver;
+    private String buttonAccountLocator = "//*[@id='header']/div/div[3]/div/div[3]/div/div/div/button";
+    private String buttonEnterLocator = "//*[@id='userToolsDropDown']/div/div[1]/div[2]/button";
+    private String inputEmailLocator = "//*[@id='login-email']";
+    private String inputPasswordLocator = "//*[@id='login-password']";
+    private String buttonEnterToLoginFormLocator = "//*[@id='default-modal']/div/div/div[2]/div/div/" +
+            "form/div/div[2]/div[3]/button";
+    private String url = "https://www.21vek.by/";
+
+    public _21vekPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+    public void getUrl(){
+        webDriver.get(url);
+        }
+    public void clickButtonAccount(){
+        webDriver.findElement(By.xpath(buttonAccountLocator)).click();
+        }
+    public void clickButtonEnter(){
+        webDriver.findElement(By.xpath(buttonEnterLocator)).click();
+        }
+    public void setInputEmail (String email){
+        webDriver.findElement(By.xpath(inputEmailLocator)).sendKeys(email);
+    }
+    public void setInputPassword(String password){
+        webDriver.findElement(By.xpath(inputPasswordLocator)).sendKeys(password);
+    }
+    public void clickButtonEnterToLoginForm(){
+        webDriver.findElement(By.xpath(buttonEnterToLoginFormLocator)).click();
+    }
+}
+

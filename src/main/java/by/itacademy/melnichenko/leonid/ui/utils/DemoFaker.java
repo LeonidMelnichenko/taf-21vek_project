4f3bb222ee0a79adb539fe514ba0890165d7c190
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-03 16:57:42 +0300

    create DemoFaker class

---
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/utils/DemoFaker.java
@@ -0,0 +1,20 @@
+package by.itacademy.melnichenko.leonid.ui.utils;
+
+import com.github.javafaker.Faker;
+
+public class DemoFaker {
+    Faker faker;
+    public DemoFaker(Faker faker) {
+        this.faker = faker;
+    }
+    public String generateFakerEmail(){
+        faker = new Faker();
+        String fakerEmail = faker.internet().emailAddress();
+        return fakerEmail;
+    }
+    public String genarateFakerPassword(){
+        faker = new Faker();
+        String fakerPassword = faker.internet().password();
+        return fakerPassword;
+    }
+}

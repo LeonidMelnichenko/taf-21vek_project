b0a7c9f29ccc6a3d60446d9da7b30ada0301a140
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-06 15:49:57 +0300

    add refactor PostObject

--- src/main/java/by/itacademy/melnichenko/leonid/api/PostObject.java
+++ src/main/java/by/itacademy/melnichenko/leonid/api/PostObject.java
@@ -3,37 +3,33 @@ package by.itacademy.melnichenko.leonid.api;
 import java.util.Objects;
 
 public class PostObject {
-    private int id;
-    private int userId;
-    private String title;
-    private String body;
+    private String email;
+    private String password;
 
-    public PostObject(int id, int userId, String title, String body) {
-        this.id = id;
-        this.userId = userId;
-        this.title = title;
-        this.body = body;
+    public PostObject(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
 
     @Override
     public String toString() {
         return "PostObject{" +
-                "id=" + id +
-                ", userId=" + userId +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PostObject that = (PostObject) o;
-        return id == that.id && userId == that.userId && Objects.equals(title, that.title) && Objects.equals(body, that.body);
+        if (!(o instanceof PostObject that)) return false;
+        return email.equals(that.email) && password.equals(that.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, userId, title, body);
+        return Objects.hash(email, password);
     }
 }
+
+
+

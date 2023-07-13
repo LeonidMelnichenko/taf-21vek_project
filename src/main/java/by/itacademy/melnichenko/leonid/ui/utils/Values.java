b6dabdbb2bf2824eb50514ad5c37405481bb66cf
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-07-13 17:36:45 +0300

    create enam

---
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/utils/Values.java
@@ -0,0 +1,15 @@
+package by.itacademy.melnichenko.leonid.ui.utils;
+
+public enum Values {
+    NO_ACCOUNT ("Нет такого аккаунта. \nЗарегистрироваться?"),
+    INVALID_FORMAT_EMAIL("Неправильный формат электронной почты"),
+    EMPTY_EMAIL ("Электронная почта не указана"),
+    EMPTY_PASSWORD ("Пароль не указан");
+    private String values;
+    Values(String values) {
+        this.values = values;
+    }
+    public String getValues() {
+        return values;
+    }
+}

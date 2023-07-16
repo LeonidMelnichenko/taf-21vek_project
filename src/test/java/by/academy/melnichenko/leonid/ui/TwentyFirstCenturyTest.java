c716e7ccaaaa4a361e434855d43bb87315377812
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-16 14:49:14 +0300

    refactor methods

--- src/test/java/by/academy/melnichenko/leonid/ui/TwentyFirstCenturyTest.java
+++ src/test/java/by/academy/melnichenko/leonid/ui/TwentyFirstCenturyTest.java
@@ -1,58 +1,34 @@
 package by.academy.melnichenko.leonid.ui;
 
-import by.itacademy.melnichenko.leonid.ui.page.TwentyFirstCenturyPage;
-import by.itacademy.melnichenko.leonid.ui.step.TwentyFirstCenturyStep;
-import by.itacademy.melnichenko.leonid.ui.utils.DemoFaker;
 import by.itacademy.melnichenko.leonid.ui.utils.Values;
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
-public class TwentyFirstCenturyTest {
-    ChromeDriver chromeDriver;
-    TwentyFirstCenturyPage page;
-    TwentyFirstCenturyStep step;
-    DemoFaker demoFaker;
+public class TwentyFirstCenturyTest extends BaseTest {
     private String validEmailAccount = "majorpayne748@gmail.com";
     private String validPasswordAccount = "TzSJ-e@C4YUqniM";
-    @BeforeEach
-    public void beforeTest(){
-        chromeDriver = new ChromeDriver();
-        chromeDriver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-        page = new TwentyFirstCenturyPage(chromeDriver);
-        step = new TwentyFirstCenturyStep(chromeDriver);
-        demoFaker = new DemoFaker(new Faker());
-        page.getUrl();
-    }
-    @AfterEach
-    public void afterTest(){
-        chromeDriver.quit();
+
+    @Test
+    public void testEnterWithValidCredentials() {
+        step.fillLoginFormAndSubmit(validEmailAccount, validPasswordAccount);
     }
 
     @Test
-    public void testEnterWithValidCredentials(){
-        step.fillLoginFormAndSubmit(validEmailAccount, validPasswordAccount);
-    }
-    @Test
-    public void testEnterWithAnyCorrectEmailAndAnyPassword(){
+    public void testEnterWithAnyCorrectEmailAndAnyPassword() {
         step.fillLoginFormAndSubmit(demoFaker.generateFakerEmail(), demoFaker.genarateFakerPassword());
         String actualResult = page.getTextAfterInvalidCredentials();
         Assertions.assertEquals(Values.NO_ACCOUNT.getValues(), actualResult);
     }
 
     @Test
-    public  void  testEnterWithIncorrectFormatOfEmailAndAnyPassword(){
+    public void testEnterWithIncorrectFormatOfEmailAndAnyPassword() {
         step.fillLoginFormAndSubmit("qwertyui", demoFaker.genarateFakerPassword());
         String actualResult = page.getResultAfterIncorrectFormatOfEmail();
         Assertions.assertEquals(Values.INVALID_FORMAT_EMAIL.getValues(), actualResult);
     }
+
     @Test
-    public void testEnterWithEmptyFields(){
+    public void testEnterWithEmptyFields() {
         step.fillLoginFormAndSubmit("", "");
         String actualResult = page.getResultWithEmptyEmail();
         String actualresult2 = page.getResultWithEmptyPassword();

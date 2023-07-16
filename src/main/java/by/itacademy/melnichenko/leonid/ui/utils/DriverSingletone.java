0979154da2f544d3ea8672d5bd788084f7fcbc8f
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-16 14:44:15 +0300

    add private constructor

--- src/main/java/by/itacademy/melnichenko/leonid/ui/utils/DriverSingletone.java
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/utils/DriverSingletone.java
@@ -1,6 +1,27 @@
 package by.itacademy.melnichenko.leonid.ui.utils;
 
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DriverSingletone {
-
-
+    private static WebDriver webDriver;
+    public static WebDriver getDriver() {
+        if(webDriver == null){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            webDriver = new ChromeDriver(chromeOptions);
+            webDriver.manage().window().maximize();
+        }
+        return webDriver;
+    }
+    private DriverSingletone() {
+    }
+    public static void quit(){
+        if(webDriver != null){
+            webDriver.quit();
+            webDriver = null;
+        }
+    }
 }

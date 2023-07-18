19854ce9ce76510ffc3d8d57b71444f94a74b6fa
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-18 23:40:16 +0300

    refactor code style

--- src/main/java/by/itacademy/melnichenko/leonid/ui/step/TwentyFirstCenturyStep.java
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/step/TwentyFirstCenturyStep.java
@@ -1,19 +1,20 @@
 package by.itacademy.melnichenko.leonid.ui.step;
 
 import by.itacademy.melnichenko.leonid.ui.page.TwentyFirstCenturyPage;
-import org.openqa.selenium.WebDriver;
 
 public class TwentyFirstCenturyStep {
-        TwentyFirstCenturyPage page;
-        public TwentyFirstCenturyStep() {
-            page = new TwentyFirstCenturyPage();
-        }
-        public  void fillLoginFormAndSubmit(String email, String password){
-                page.clickButtonCookies();
-                page.clickButtonAccount();
-                page.clickButtonEnter();
-                page.setInputEmail(email);
-                page.setInputPassword(password);
-                page.clickButtonEnterToLoginForm();
-        }
+    TwentyFirstCenturyPage page;
+
+    public TwentyFirstCenturyStep() {
+        page = new TwentyFirstCenturyPage();
     }
+
+    public void fillLoginFormAndSubmit(String email, String password) {
+        page.clickButtonCookies();
+        page.clickButtonAccount();
+        page.clickButtonEnter();
+        page.setInputEmail(email);
+        page.setInputPassword(password);
+        page.clickButtonEnterToLoginForm();
+    }
+}

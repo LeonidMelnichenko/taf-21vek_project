98110ed702b426157b969991feef2bc3d9d29a26
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-18 23:00:08 +0300

    create UserAccount class

---
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/domain/UserAccount.java
@@ -0,0 +1,13 @@
+package by.itacademy.melnichenko.leonid.ui.domain;
+
+public class UserAccount {
+    private static String validEmailAccount = "majorpayne748@gmail.com";
+    private static String validPasswordAccount = "TzSJ-e@C4YUqniM";
+
+    public  String getValidEmailAccount() {
+        return validEmailAccount;
+    }
+    public String getValidPasswordAccount() {
+        return validPasswordAccount;
+    }
+}

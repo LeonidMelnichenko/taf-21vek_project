8eb8221610c5c65015e7e115a827cb15173878fe
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-18 23:01:23 +0300

    refactor enum Values to class Errors

---
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/domain/Errors.java
@@ -0,0 +1,8 @@
+package by.itacademy.melnichenko.leonid.ui.domain;
+
+public class Errors {
+    public static final String NO_ACCOUNT = "Нет такого аккаунта. \nЗарегистрироваться?";
+    public static final String INVALID_FORMAT_EMAIL = "Неправильный формат электронной почты";
+    public static final String EMPTY_EMAIL = "Электронная почта не указана";
+    public static final String EMPTY_PASSWORD = "Пароль не указан";
+}

d985da69385725e229593a3f6d3e8a6744908d89
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-07-19 12:39:23 +0300

    add refactor fields

---
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/domain/User.java
@@ -0,0 +1,7 @@
+package by.itacademy.melnichenko.leonid.ui.domain;
+
+public class User {
+    public static final String VALID_EMAIL_ACCOUNT = "majorpayne748@gmail.com";
+    public static final String VALID_PASSWORD_ACCOUNT = "TzSJ-e@C4YUqniM";
+    public static final String INCORRECT_FORMAT_EMAIL = "qwertyui";
+}

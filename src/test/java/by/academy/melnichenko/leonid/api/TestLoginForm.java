1d1a94f395b22a220f2ac066686099ec0f6b69ea
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-07-20 12:41:32 +0300

    add refactor test methods

--- src/test/java/by/academy/melnichenko/leonid/api/TestLoginForm.java
+++ src/test/java/by/academy/melnichenko/leonid/api/TestLoginForm.java
@@ -1,59 +1,33 @@
 package by.academy.melnichenko.leonid.api;
 
-import by.itacademy.melnichenko.leonid.api.Headers;
-import by.itacademy.melnichenko.leonid.api.requests.Request;
-import io.restassured.response.ValidatableResponse;
-import org.testng.Assert;
+import by.itacademy.melnichenko.leonid.api.PageAPI;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TestLoginForm {
-    private String urlLoginForm = "https://www.21vek.by/users/login/";
-    Request request = new Request();
+    PageAPI page = new PageAPI();
+
     @Test
     public void testEnterWithValidСredentials() {
-        String body = "{\n" +
-                "    \"User\": {\n" +
-                "        \"email\": \"majorpayne748@gmail.com\",\n" +
-                "        \"password\": \"TzSJ-e@C4YUqniM\"\n" +
-                "    }\n" +
-                "}";
-        given().when().headers(request.getLoginHeadersToCredentials()).body(body).post(urlLoginForm)
-                .then().assertThat()
+        page.getResponseValidCreds().assertThat()
                 .statusCode(302)
                 .header("content-type", equalTo("text/html; charset=UTF-8"))
                 .header("location", equalTo("https://www.21vek.by/"));
     }
+
     @Test
     public void testEnterWithInvalidСredentials() {
-        String body = "{\n" +
-                "    \"User\": {\n" +
-                "        \"email\": \"qwerty@gmail.com\",\n" +
-                "        \"password\": \"12321312312312\"\n" +
-                "    }\n" +
-                "}";
-        given().when().headers(request.getLoginHeadersToCredentials()).body(body).post(urlLoginForm)
-                .then()
-                .assertThat()
+        page.getResponseInvalidCreds().assertThat()
                 .statusCode(200)
                 .header("content-type", equalTo("application/json"))
                 .body("error", equalTo("Неправильный пароль"));
     }
+
     @Test
     public void testEnterWithEmptyFields() {
-        String body = "{\n" +
-                "    \"User\": {\n" +
-                "        \"email\": \"\",\n" +
-                "        \"password\": \"\"\n" +
-                "    }\n" +
-                "}";
-        given().when().headers(request.getLoginHeadersToEmptyFields()).body(body).post(urlLoginForm)
-                .then()
-                .assertThat()
+        page.getResponseWithEmptyFields().assertThat()
                 .statusCode(404)
                 .header("content-type", equalTo("text/html"));
     }

e7da35fc79c514a0bec40a6722cafb0849fca5eb
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-07-20 16:24:11 +0300

    add methods to search

--- src/main/java/by/itacademy/melnichenko/leonid/api/PageAPI.java
+++ src/main/java/by/itacademy/melnichenko/leonid/api/PageAPI.java
@@ -8,6 +8,8 @@ import static io.restassured.RestAssured.given;
 
 public class PageAPI {
     private String urlLoginForm = "https://www.21vek.by/users/login/";
+    private String subjectToSearch = "https://www.21vek.by/tv/";
+    private String invalidValuesToSearch = "https://www.21vek.by/search/?sa=&term=%40%23%24%25%25%25%25%25%25";
     UserAPI userAPI = new UserAPI();
 
     public HashMap<String, String> getLoginHeadersToCredentials() {
@@ -41,5 +43,26 @@ public class PageAPI {
                 .body(userAPI.getEmptyFields())
                 .post(urlLoginForm)
                 .then();
+    }
+    public HashMap<String, String> getSearchHeaders(){
+        HashMap<String, String> searchHeaders = new HashMap<>();
+        searchHeaders.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like " +
+                "Gecko) Chrome/109.0.0.0 Safari/537.36");
+        return searchHeaders;
+    }
+    public String getSearchParams(){
+        HashMap<String, String> searchParams = new HashMap<>();
+        searchParams.put("sa", "");
+        searchParams.put("term", "%40%23%24%25%25%25%25%25%25");
+        return String.valueOf(searchParams);
+    }
+    public ValidatableResponse getSearchSomething(){
+        return given().when().headers(getSearchHeaders())
+                .get(subjectToSearch).then();
+    }
+
+    public ValidatableResponse getSearchWithInvalidValues(){
+        return given().when().queryParam(getSearchParams()).headers(getSearchHeaders())
+                .get(invalidValuesToSearch).then();
     }
 }

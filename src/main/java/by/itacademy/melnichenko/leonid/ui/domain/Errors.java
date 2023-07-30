99d90f6fc1d104bf6677668f2722c6adf0ebf2ac
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-07-30 14:54:56 +0300

    commit for jenkins integration test revert

--- src/main/java/by/itacademy/melnichenko/leonid/ui/domain/Errors.java
+++ src/main/java/by/itacademy/melnichenko/leonid/ui/domain/Errors.java
@@ -4,5 +4,5 @@ public class Errors {
     public static final String NO_ACCOUNT = "Нет такого аккаунта. \nЗарегистрироваться?";
     public static final String INVALID_FORMAT_EMAIL = "Неправильный формат электронной почты";
     public static final String EMPTY_EMAIL = "Электронная почта не указана";
-    public static final String EMPTY_PASSWORD = "Пароль не указан!";
+    public static final String EMPTY_PASSWORD = "Пароль не указан";
 }
